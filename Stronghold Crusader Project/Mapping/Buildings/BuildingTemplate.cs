00a6867678653bbc762036e137e88482ec043b80
filippos <AQ241177@aquinas.ac.uk>
2025-06-20 14:21:02 +0100

    Revert "test comit"
    This reverts commit 7bd914ba96eed5879a1ca567038b867c3c785d92.

--- Stronghold Crusader Project/Mapping/Buildings/BuildingTemplate.cs
+++ Stronghold Crusader Project/Mapping/Buildings/BuildingTemplate.cs
@@ -1,15 +1,10 @@
-using System.Collections.Generic;
-using Stronghold_Crusader_Project.Materials;
-
 namespace Stronghold_Crusader_Project.Mapping.Buildings;
 
 public abstract class BuildingTemplate
 {
     string Name { get; set; }
     int GoldCost  { get; set; }
-    List<MaterialTemplate>  MaterialRequired { get; set; }
     int Health  { get; set; }
-    int MaxHealth { get; set; }
     
     
     
