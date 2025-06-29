ae6bccf797e96f71c6681af5429536fa1e072360
filippos <AQ241177@aquinas.ac.uk>
2025-06-29 10:33:37 +0100

    Started revamping assets and added/changed some code to make revamping assets easier

--- Other/SetupCode/File Name Converter/File Name Converter/Program.cs
+++ Other/SetupCode/File Name Converter/File Name Converter/Program.cs
@@ -12,8 +12,51 @@ namespace File_Name_Converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the folder path");
-            string FolderPath = Console.ReadLine();
+            while (true)
+            {
+                Console.WriteLine("Enter the folder path");
+                string FolderPath = Console.ReadLine();
+                FolderPath = FolderPath.Replace("'", "");
+                int ActiveSlashPosition = FolderPath.LastIndexOf("/");
+                string FolderName = FolderPath.Substring(ActiveSlashPosition + 1) + "X";
+                if (Directory.Exists(FolderPath))
+                {
+                    DisplayFiles(FolderPath);
+                    Console.WriteLine("Enter what the current file name");
+                    string CurrentFileName = Console.ReadLine();
+                    Console.WriteLine("Enter the file extension");
+                    string FileExtension = Console.ReadLine();
+                    Console.WriteLine("Enter the number it starts on");
+                    int OffSet = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    Console.WriteLine("1. Ascending item to folder name");
+                    Console.WriteLine("2. Descending item to folder name");
+                    Console.WriteLine("3. Directional item");
+                    int Input = int.Parse(Console.ReadLine());
+                    if (Input == 1 || Input == 2)
+                    {
+                        Console.WriteLine("Enter the new name you want without the count");
+                        string NewFileName = Console.ReadLine();
+                        switch (Input)
+                        {
+                            case 1:
+                                ChangeFileNames(FolderPath, CurrentFileName, NewFileName, FileExtension,OffSet, false);
+                                break;
+                            case 2:
+                                ChangeFileNames(FolderPath, CurrentFileName, NewFileName, FileExtension,OffSet, true);
+                                break;
+                        }
+                    }
+                }
+                Console.ReadLine();
+                Thread.Sleep(100);
+                Console.Clear();
+            }
+        }
+        static void DisplayFiles(string FolderPath)
+        {
+            Thread.Sleep(200);
+            Console.Clear();
             if (Directory.Exists(FolderPath))
             {
                 Thread.Sleep(1000);
@@ -22,38 +65,41 @@ namespace File_Name_Converter
                 string[] FilesFound = Directory.GetFiles(FolderPath);
                 foreach (string ActiveFile in FilesFound)
                 {
+                    Thread.Sleep(100);
                     Console.WriteLine(Path.GetFileName(ActiveFile));
                 }
-                Console.WriteLine("Enter what the current file name is without the count");
-                string CurrentFileName = Console.ReadLine();
-                Console.WriteLine("Enter the new name you want without the count");
-                string NewFileName = Console.ReadLine();
-                Console.WriteLine("Enter the file extension");
-                string FileExtension = Console.ReadLine();
-                string OldFilePath = Path.Combine(FolderPath, CurrentFileName);
-                string NewFilePath = Path.Combine(FolderPath, NewFileName);
-                Console.WriteLine(File.Exists(OldFilePath + '0' + FileExtension));
-                Thread.Sleep(200);
-                Console.Clear();
-                for (int Count = 0; File.Exists(OldFilePath + Count.ToString() + FileExtension); Count++)
-                {
-                    string OldFullFilePath = OldFilePath + Count.ToString() + FileExtension;
-                    string NewFullFilePath = NewFilePath + (Count).ToString() + FileExtension;
-                    Console.WriteLine($"{Path.GetFileName(OldFullFilePath)} -> {Path.GetFileName(NewFullFilePath)}");
-                    Thread.Sleep(300);
-
-                    File.Move(OldFullFilePath, NewFullFilePath);
-                }
-                Thread.Sleep(2000);
-                Console.Clear();
-                Console.WriteLine("Done");
-                Console.ReadLine();
             }
             else
             {
-                Console.WriteLine("Invalid Directory");
+                Console.WriteLine("No Files Found");
             }
-            Console.ReadLine();;
+        }
+        static void ChangeFileNames(string FolderPath, string OldName, string NewName, string FileExtension, int OffSet, bool Reverse)
+        {
+            Console.Clear();
+            string OldFilePath = Path.Combine(FolderPath, OldName);
+            string NewFilePath = Path.Combine(FolderPath, NewName + "X");
+            int CountDifference = 1;
+            if (Reverse == true)
+            {
+                CountDifference = -1;
+            }
+            int TempNumber = OffSet;
+            for (int Count = 0; File.Exists(OldFilePath + (TempNumber + CountDifference).ToString() + FileExtension); Count++)
+            {
+                Thread.Sleep(200);
+                TempNumber = OffSet + Count;
+                if (Reverse == true)
+                {
+                    TempNumber = OffSet - Count;
+                }
+                string OldFullFilePath = OldFilePath + TempNumber.ToString() + FileExtension;
+                string NewFullFilePath = NewFilePath + (Count + 1).ToString() + FileExtension;
+                Console.WriteLine($"{Path.GetFileName(OldFullFilePath)} -> {Path.GetFileName(NewFullFilePath)}");
+                File.Move(OldFullFilePath, NewFullFilePath);
+            }
+            Console.WriteLine(TempNumber);
+            Console.WriteLine("Finished");
         }
     }
-}
+} 
