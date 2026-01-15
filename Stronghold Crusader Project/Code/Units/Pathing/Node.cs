28a3d704b92ca8510eba7f0b1e4d1e60cb0a1bc1
filippos <AQ241177@aquinas.ac.uk>
2026-01-15 08:26:01 +0000

    Started recoding the path finding

--- Stronghold Crusader Project/Code/Units/Pathing/Node.cs
+++ Stronghold Crusader Project/Code/Units/Pathing/Node.cs
@@ -1,49 +1,37 @@
-namespace Stronghold_Crusader_Project.Code.PathFinding;
+namespace Stronghold_Crusader_Project.Code.Units.PathFinding;
+
+/// <summary>
+/// Node is used to represent a possible position when using path finding
+/// 
+/// </summary>
 
 public class Node
 {
-    public Vector2 Position {get; set;}
-    public Node PreviousNode {get; set;}
-    public float CostFromStart  {get; set;}
-    public float CostFromEnd {get; set;}
+    //Class Variables
+    
+    public Vector2 Position;
+    public Node PreviousNode;
+    public float CostFromStart;
+    public float CostFromEnd;
     public float TotalCost => CostFromStart + CostFromEnd;
 
-    public Node(Vector2 position, float costFromStart, float costFromEnd)
-    {
-        Position = position;
-        CostFromStart = costFromStart;
-        CostFromEnd = costFromEnd;
-    }
+    //Class Methods
 
-    public static float EstimatedDistance(Vector2 Start, Vector2 End) //Estimates the distance between two points
+    public Node(Vector2 InputPosition, float InputCostFromStart, float InputCostFromEnd, Node InputPreviousNode)
     {
-        //Octile distance - diagonal is not an equal move to straight moves
-        int DifferentX = Math.Abs((int)Start.X - (int)End.X); 
-        int DifferentY = Math.Abs((int)Start.Y - (int)End.Y); //Getting differences in positions
-        float StraightCost = 1; //Amount it costs to move straight
-        float DiagnalCost = 1.414f; //Amount it costs to move diagnally
-        int Min = Math.Min(DifferentX, DifferentY); //Biggest Number
-        int Max = Math.Min(DifferentX, DifferentY); //Smallest Number
-        return StraightCost * (Max - Min) + DiagnalCost * Min; //Octile distance formula
-    }
-
-    public override bool Equals(object PossibleNode) //Ovveriding default check as it doesn't work as it won't work when checking the positions of the nodes to make sure we don't get duplicates
-    {
-        if (PossibleNode is not Node) //Checking if it is even a node
-        {
-            return false;
-        }
-        Node CheckNode = (Node)PossibleNode; //Converting it from an object into a node class
-        if (CheckNode.Position.X != Position.X || CheckNode.Position.Y != Position.Y) //Checking if the positions are equal
-        {
-            return false;
-        }
-        return true;
-    }
-
-    public override int GetHashCode() //Creating a hash code for the position of the node
-    {
-        return HashCode.Combine(Position.X, Position.Y);
+        Position = InputPosition;
+        CostFromStart = InputCostFromStart;
+        CostFromEnd = InputCostFromEnd;
+        PreviousNode = InputPreviousNode;
     }
     
+    #region Public Facing
+    //Classes that can be used outside of this class
+
+    public static float EstimatedDistance(Vector2 Start, Vector2 End) //Estimates the distance between two vectors
+    {
+        
+    }
+    
+    #endregion
 }
